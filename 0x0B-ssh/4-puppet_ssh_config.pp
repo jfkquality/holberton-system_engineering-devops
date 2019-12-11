cf11b408ef6f94d340b9fa3e7522d24e2e8d4a5e
jfkquality <jfkquality@gmail.com>
2019-12-11 05:03:03 +0000

    0x0B SSH. Task 4. Another shot.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,25 @@
 # set up ssh config file; ~/.ssh/holberton; no password
+exec { 'identity':
+  command  => 'echo IdentityFile ~/.ssh/holberton >> /etc/ssh/ssh_config',
+  provider => 'shell',
+  path     => '/usr/bin/',
+}
+exec { 'no_password':
+  command  => 'echo IdentityFile ~/.ssh/holberton >> /etc/ssh/ssh_config',
+  provider => 'shell',
+  path     => '/usr/bin/',
+}
 
-file_line { 'holberton_identity':
-  ensure            => absent,
-  path              => '/etc/ssh/ssh_config',
-  line              => 'PasswordAuthentication yes',
-  match_for_absence => true,
-}
-file_line { 'no_password':
-  ensure            => absent,
-  path              => '/etc/ssh/ssh_config',
-  line              => 'IdentityFile ~/.ssh/holberton',
-  match_for_absence => true,
-}
+# file_line { 'holberton_identity':
+#   ensure            => absent,
+#   path              => '/2-ssh_config',
+#   line              => 'PasswordAuthentication yes',
+#   match_for_absence => true,
+# }
+# file_line { 'no_password':
+#   ensure            => absent,
+#   path              => '/etc/ssh/ssh_config',
+#   path              => '2-ssh_config',
+#   line              => 'IdentityFile ~/.ssh/holberton',
+#   match_for_absence => true,
+# }
