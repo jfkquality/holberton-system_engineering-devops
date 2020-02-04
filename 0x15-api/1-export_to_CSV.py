9b40d07b5b7fc0efb5e9deb5c504517e3d3268cd
jfkquality <jfkquality@gmail.com>
2020-02-04 05:07:36 +0000

    Task 1. Init commit.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Get completed tasks for user id."""
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """"Get completed tasks for user id."""
+    userid = int(argv[1])
+
+    payload = {'id': userid}
+    url1 = 'https://jsonplaceholder.typicode.com/users/'
+    r_user = requests.get(url1, params=payload)
+    user = json.loads(r_user.text)  # or  r_user.json()
+
+    payload = {'userId': userid}
+    url2 = 'https://jsonplaceholder.typicode.com/users/user["id"]/todos'
+    r_todos = requests.get(url2, params=payload)
+    todos = json.loads(r_todos.text)  # or r_todos.json()
+
+    f = csv.writer(open("USER_ID.csv", "wb+"))
+
+    for todo in todos:
+        f.writerow([todo["userId"],
+                    user[0]["username"],
+                    todo["complete"],
+                    todo["title"]])
+
+    # total = len(todos[0])
+    # total = 0
+    # complete = 0
+    # completed = []
+    # for todo in todos:
+    #     if todo["completed"]:
+    #         completed.append(todo["title"])
+    #         complete += 1
+    #     total += 1
+    # print("Employee {} is done with tasks({}/{}):".format(user[0]["name"],
+    #                                                       complete, total))
+    # for done in completed:
+    #     print("\t {}".format(done))
