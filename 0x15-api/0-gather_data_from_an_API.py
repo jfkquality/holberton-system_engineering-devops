534d52c5fb76bc6baa894a59e4aa8d40982333df
jfkquality <jfkquality@gmail.com>
2020-02-04 04:52:08 +0000

    Task 0. Delete debug print line.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """Get completed tasks for user id."""
-from sys import argv
 import json
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
     """"Get completed tasks for user id."""
     userid = int(argv[1])
+
     payload = {'id': userid}
     url1 = 'https://jsonplaceholder.typicode.com/users/'
     r_user = requests.get(url1, params=payload)
     user = json.loads(r_user.text)  # or  r_user.json()
-    print("USER NAME IS:", user[0]["name"])
+
     payload = {'userId': userid}
     url2 = 'https://jsonplaceholder.typicode.com/users/user["id"]/todos'
     r_todos = requests.get(url2, params=payload)
-    todos = r_todos.json()
+    todos = json.loads(r_todos.text)  # or r_todos.json()
     total = len(todos[0])
     complete = 0
     completed = []
