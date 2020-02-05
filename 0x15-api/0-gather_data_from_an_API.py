f994a2d3df3b0d19693bc80ed69e58ed42e51f0a
jfkquality <jfkquality@gmail.com>
2020-02-05 19:33:39 +0000

    Refactor tasks 0, 1: use res.json() vs json.loads. Task 1: Correct filename.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,22 +12,23 @@ if __name__ == "__main__":
     payload = {'id': userid}
     url1 = 'https://jsonplaceholder.typicode.com/users/'
     r_user = requests.get(url1, params=payload)
-    user = json.loads(r_user.text)  # or  r_user.json()
+    # user = json.loads(r_user.text)  # or  r_user.json()
+    user = r_user.json()
 
     payload = {'userId': userid}
     # url2 = 'https://jsonplaceholder.typicode.com/users/user["id"]/todos'
     url2 = 'https://jsonplaceholder.typicode.com/users/1/todos'
     r_todos = requests.get(url2, params=payload)
-    todos = json.loads(r_todos.text)  # or r_todos.json()
-    # total = len(todos[0])
-    total = 0
+    # todos = json.loads(r_todos.text)  # or r_todos.json()
+    todos = r_todos.json()
+    total = len(todos)
     complete = 0
     completed = []
     for todo in todos:
         if todo["completed"]:
             completed.append(todo["title"])
             complete += 1
-        total += 1
+        # total += 1
     print("Employee {} is done with tasks({}/{}):".format(user[0]["name"],
                                                           complete, total))
     for done in completed:
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -17,11 +17,12 @@ if __name__ == "__main__":
     user = json.loads(r_user.text)  # or  r_user.json()
 
     payload = {'userId': userid}
-    url2 = 'https://jsonplaceholder.typicode.com/users/user["id"]/todos'
+    url2 = 'https://jsonplaceholder.typicode.com/users/1/todos'
     r_todos = requests.get(url2, params=payload)
-    todos = json.loads(r_todos.text)  # or r_todos.json()
-
-    f = csv.writer(open("USER_ID.csv", "w"), quoting=csv.QUOTE_ALL)
+    # todos = json.loads(r_todos.text)  # or r_todos.json()
+    todos = r_todos.json()
+    # fname = todo['userID'] + '.csv'
+    f = csv.writer(open(str(userid) + ".csv", "w"), quoting=csv.QUOTE_ALL)
     # headers = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
     # f.writerow(headers)
 
