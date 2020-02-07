9c3cfe4277acaf1b66d08de5e6d57cb007c4080b
jfkquality <jfkquality@gmail.com>
2020-02-07 02:03:58 +0000

    Task 1. Init commit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Query Reddit and return top ten posts of passed endpoint."""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """Query Reddit and return total subscribers of passed endpoint."""
+    base_url = 'https://www.reddit.com'
+    query = '/r/' + argv[1] + '/hot.json'
+    if len(argv) < 2:
+        print('None')
+        return (0)
+    res = requests.get(base_url+query, headers={'User-agent': 'your bot 0.1'})
+    if res.status_code != 200:
+        print('None')
+        exit
+    about = res.json()
+    posts = about['data']['children']
+    i = 0
+    while i < 10:
+        print(posts[i]['data']['title'])
+        i += 1
