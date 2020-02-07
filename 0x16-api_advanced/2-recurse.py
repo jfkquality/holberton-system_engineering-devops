79ae369016143d2108d29ff4d71f93eff62d26f5
jfkquality <jfkquality@gmail.com>
2020-02-07 07:21:22 +0000

    Task 2. Use limit parameter to avoid memory error.

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,42 +11,31 @@ def create_list(hot_list, posts, posts_len):
         # print(posts[i]['data']['title'])
         i += 1
     return (hot_list)
-    # hot_list.append(posts[posts_len - 1]['data']['title'])
-    # print(posts[posts_len - 1]['data']['title'])
 
 
 def recurse(subreddit, hot_list=[], nextpage=None):
     """Query Reddit recursively and return all posts of passed endpoint."""
 
-    # print("HOT LIST IS EMPTY")
     if len(argv) < 2:
         return (None)
 
     base_url = 'https://www.reddit.com'
-    query = '/r/' + argv[1] + '/hot.json?after=nextpage'
-    print(base_url + query)
-    res = requests.get(base_url+query, headers={'User-agent': 'your bot 0.1'})
+    query = '/r/' + argv[1] + '/hot.json'  # ?after=nextpage'
+    payload = {'after': nextpage, 'limit': 100}
+    res = requests.get(base_url+query, params=payload,
+                       headers={'User-agent': 'your bot 0.1'})
     if res.status_code != 200:
         return (None)
-    print(res.headers.get('Content-Type'))
     about = res.json()
     posts = about['data']['children']
     posts_len = len(posts)
-    print("POSTS LENGTH ", posts_len)
 
     if posts_len == 0:
         return (None)
     hot_list.extend(create_list(hot_list, posts, posts_len))
-    # nextpage = posts[posts_len - 1]['data']['after']
     nextpage = about['data']['after']
-    print("NEXT PAGE ", nextpage)
 
     if nextpage:
         (recurse(subreddit, hot_list, nextpage))
-
-    # while i < posts_len:
-    #     # hot_list.append(posts[i]['data']['title'])
-    #     print(posts[i]['data']['title'])
-    #     i += 1
 
     return (hot_list)
