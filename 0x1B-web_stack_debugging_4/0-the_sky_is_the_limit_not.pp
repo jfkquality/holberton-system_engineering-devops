55a57d5f4f369b5e6c90200c139d9e051ba75238
jfkquality <jfkquality@gmail.com>
2020-02-28 02:01:29 +0000

    0x1B Web debug 4, Task 0. Init commit.

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,28 @@
+# Puppet manifest to correct 'too many files open'
+
+}
+exec { 'ulimit':
+  command  => 'sed -i "s/n 15/n 1500/" /etc//default/nginx',
+  provider => 'shell',
+  path     => '/usr/bin/',
+  before   => Exec["nginx-restart"],
+}
+exec { 'nginx-restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+  path     => '/usr/bin/',  # sbin/nginx',
+  require   => Exec["ulimit"],
+}
+# exec { 'nginx-start':
+#   command     => 'nginx',
+#   provider    => 'shell',
+#   path        => '/usr/sbin',  # sbin/nginx',
+#   require     => Exec["nginx-stop"],
+# }
+
+# file_line { 'holberton_identity':
+#   ensure   => absent,
+#   path     => '/etc/nginx/nginx.conf',
+#   line     => 'worker_rlimit_nofile 3072;',
+#   match    => ' ',
+# }
